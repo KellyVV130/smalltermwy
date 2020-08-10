85d4ef94aacd311c95e06f5420e20bb8a78d106b
lancathyy <lan_cathy@163.com>
2020-08-10 21:46:14 +0800

    增加文档编辑, 删除错误放置到globalfront外的文件

---
+++ globalfront/src/views/EditorPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <editor-bar
+      v-model="detail"
+      :isClear="isClear"
+      @change="change"
+    ></editor-bar>
+
+    <button @click="send">点我！</button>
+  </div>
+</template>
+<script>
+import EditorBar from "../components/public/editor";
+
+export default {
+  components: { EditorBar },
+  data() {
+    return {
+      isClear: false,
+      detail: ""
+    };
+  },
+  methods: {
+    change(val) {
+      console.log(val);
+    },
+    send() {
+      alert(this.detail);
+    }
+  }
+};
+</script>
+
+<style>
+  .basic{
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: whitesmoke;
+  }
+</style>
