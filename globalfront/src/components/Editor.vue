910839b6aabe79230701e719724b5bbdeb2772c4
lancathyy <lan_cathy@163.com>
2020-08-13 17:33:36 +0800

    简单的编辑文档界面

--- globalfront/src/components/Editor.vue
+++ globalfront/src/components/Editor.vue
@@ -7,23 +7,23 @@
 
 <script>
 // import E from "wangeditor";
-const E = process.browser ? require("wangeditor") : undefined;
+const E = process.browser ? require('wangeditor') : undefined
 export default {
   data() {
     return {
       // uploadPath,
       editor: null,
       info_: null
-    };
+    }
   },
   model: {
-    prop: "value",
-    event: "change"
+    prop: 'value',
+    event: 'change'
   },
   props: {
     value: {
       type: String,
-      default: ""
+      default: ''
     },
     isClear: {
       type: Boolean,
@@ -34,25 +34,25 @@ export default {
     isClear(val) {
       // 触发清除文本域内容
       if (val) {
-        this.editor.txt.clear();
-        this.info_ = null;
+        this.editor.txt.clear()
+        this.info_ = null
       }
     },
     value: function(value) {
       if (value !== this.editor.txt.html()) {
-        this.editor.txt.html(this.value);
+        this.editor.txt.html(this.value)
       }
     }
   },
   mounted() {
-    this.seteditor();
-    this.editor.txt.html(this.value);
+    this.seteditor()
+    this.editor.txt.html(this.value)
   },
   methods: {
     seteditor() {
       // http://192.168.2.125:8080/admin/storage/create
-      this.editor = new E(this.$refs.toolbar, this.$refs.editor);
-      this.editor.customConfig.uploadImgShowBase64 = true; // base 64 存储图片
+      this.editor = new E(this.$refs.toolbar, this.$refs.editor)
+      this.editor.customConfig.uploadImgShowBase64 = true // base 64 存储图片
       // this.editor.customConfig.uploadImgServer =
       //   ""; // 配置服务器端地址
       // this.editor.customConfig.uploadImgHeaders = {}; // 自定义 header
@@ -63,60 +63,43 @@ export default {
 
       // 配置菜单
       this.editor.customConfig.menus = [
-        "head", // 标题
-        "bold", // 粗体
-        "fontSize", // 字号
-        "fontName", // 字体
-        "italic", // 斜体
-        "underline", // 下划线
-        "strikeThrough", // 删除线
-        "foreColor", // 文字颜色
-        "backColor", // 背景颜色
-        "link", // 插入链接
-        "list", // 列表
-        "justify", // 对齐方式
-        "quote", // 引用
-        "emoticon", // 表情
-        "image", // 插入图片
-        "table", // 表格
-        "video", // 插入视频
-        "code", // 插入代码
-        "undo", // 撤销
-        "redo", // 重复
-        "fullscreen" // 全屏
-      ];
+        'head', // 标题
+        'bold', // 粗体
+        'fontSize', // 字号
+        'fontName', // 字体
+        'italic', // 斜体
+        'underline', // 下划线
+        'strikeThrough', // 删除线
+        'foreColor', // 文字颜色
+        'backColor', // 背景颜色
+        'link', // 插入链接
+        'list', // 列表
+        'justify', // 对齐方式
+        'quote', // 引用
+        'emoticon', // 表情
+        'image', // 插入图片
+        'table', // 表格
+        'video', // 插入视频
+        'code', // 插入代码
+        'undo', // 撤销
+        'redo', // 重复
+        'fullscreen' // 全屏
+      ]
 
-      this.editor.customConfig.uploadImgHooks = {
-        fail: (xhr, editor, result) => {
-          // 插入图片失败回调
-        },
-        success: (xhr, editor, result) => {
-          // 图片上传成功回调
-        },
-        timeout: (xhr, editor) => {
-          // 网络超时的回调
-        },
-        error: (xhr, editor) => {
-          // 图片上传错误的回调
-        },
-        customInsert: (insertImg, result, editor) => {
-          // 图片上传成功，插入图片的回调
-        }
-      };
       this.editor.customConfig.onchange = html => {
-        this.info_ = html; // 绑定当前逐渐地值
-        this.$emit("change", this.info_); // 将内容同步到父组件中
-      };
+        this.info_ = html // 绑定当前逐渐地值
+        this.$emit('change', this.info_) // 将内容同步到父组件中
+      }
       // 创建富文本编辑器
-      this.editor.create();
+      this.editor.create()
     }
   }
-};
+}
 </script>
 
 <style lang="css">
 .editor {
-  width: 100%;
+  width: 50%;
   margin: 0 auto;
   position: relative;
   z-index: 0;
@@ -126,6 +109,6 @@ export default {
 }
 .text {
   border: 1px solid #ccc;
-  min-height: 500px;
+  min-height: 1000px;
 }
 </style>
