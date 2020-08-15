f2175c9e7d30e40a860abeee80e47d79f43c250e
whisperzqh <1214960505@qq.com>
2020-08-15 22:21:15 +0800

    查看未读消息 单个消息已读 单个消息删除 查看全部消息 全部消息已读 删除全部消息

--- globalfront/src/components/header.vue
+++ globalfront/src/components/header.vue
@@ -16,14 +16,14 @@
             <el-table-column width="200" property="content"></el-table-column>
             <el-table-column width="40">
               <template slot-scope="scope">
-                <el-tooltip effect="dark" content="拒绝加入团队" placement="bottom">
+                <el-tooltip effect="dark" content="删除" placement="bottom">
                 <i class="el-icon-close" @click="refuseMessage(scope.row)" style="cursor: pointer;"></i>
                 </el-tooltip>
               </template>
             </el-table-column>
             <el-table-column width="40">
               <template slot-scope="scope">
-                <el-tooltip effect="dark" content="设为已读" placement="bottom">
+                <el-tooltip effect="dark" content="标为已读" placement="bottom">
                 <i class="el-icon-check" @click="checkMessage(scope.row)" style="cursor: pointer;"></i>
                 </el-tooltip>
               </template>
@@ -43,6 +43,7 @@
 </template>
 
 <script>
+import {getMessage} from '../api/api'
   export default {
     name: "brain",
     data(){
@@ -50,21 +51,63 @@
         userImg:'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png',
         hasMessage:true,
         gridData: [{
+          id: 1,
           date: '2020/08/11',
           name: 'whisper',
+          type: 1,
           content: '添加你为[小学期]协作者'
         }, {
+          id: 2,
           date: '2016/05/04',
           name: 'sunny',
           content: '评论了[小学期]'
-        },{
-          date: '2020/08/11',
-          name: 'whisper',
-          content: '添加你为[小学期]协作者'
         }]
       }
     },
     methods:{
+      //获取未读消息
+      getMessage(){
+        getMessage().then(response => {
+          if(response.status===200){
+            console.log(response)
+            let i=0
+            for(i=0;i<response.data.length;i++){
+              this.gridData[i].id=response.data[i].id
+              this.gridData[i].date=response.data[i].time
+              this.gridData[i].name=response.data[i].origin_user//origin_user是谁发出这个消息
+              this.gridData[i].type=response.data[i].type
+              if(response.data[i].type===1){//1:团队邀请信息（给被邀请人发）
+                this.gridData[i].content='邀请你加入团队'
+              }
+              else if(response.data[i].type===2){//2:加入团队结果（给邀请人+老大发）
+                this.gridData[i].content='回复了你的团队邀请'
+              }
+              else if(response.data[i].type===3){//3:退出团队结果（给老大发）
+                this.gridData[i].content='退出了团队'
+              }
+              else if(response.data[i].type===4){//4:被踢出团队提醒（给被踢的人发）
+                this.gridData[i].content='将你移出团队'
+              }
+              else if(response.data[i].type===5){//5:被加入协作者提醒（给被邀请人发）
+                this.gridData[i].content='邀请你成为协作者'
+              }
+              else if(response.data[i].type===6){//6:文档被评论（给被评论的文档的创建者+协作者发）
+                this.gridData[i].content='评论了你的文档'
+              }
+              else if(response.data[i].type===7){//7:评论被回复（给被回复的评论人发）
+                this.gridData[i].content='回复了你的评论'
+              }
+              else if(response.data[i].type===8){//8:退出文档协作者结果（给创建者发）
+                this.gridData[i].content='退出了文档协作者'
+              }
+              else if(response.data[i].type===9){//9：被踢出文档协作者提醒（给被踢的人发）
+                this.gridData[i].content='将你移出文档协作者'
+              }
+            }
+          }
+        })
+      },
+      //控制表格颜色 舍弃？
       tableRowClassName({rowIndex}){
         if(rowIndex===1) return 'warning-row';
         else return 'success-row';
@@ -78,6 +121,9 @@
       toPerson(){
         this.$router.push({name: 'PersonInfo', params:{personId:localStorage.userId}})
       }
+    },
+    mounted(){
+      this.getMessage()
     }
   }
 </script>
--- globalfront/src/components/messageCard.vue
+++ globalfront/src/components/messageCard.vue
@@ -4,20 +4,26 @@
   <el-card  style="width:500px">
     <el-container>
       <el-col span="3">
-          <i v-if="item.type==='comment'" class="el-icon-chat-square" style="margin-top:40%; margin-left:10px"></i>
-          <i v-if="item.type==='delete'" class="el-icon-close" style="margin-top:40%; margin-left:10px"></i>
-          <i v-if="item.type==='share'" class="el-icon-share" style="margin-top:40%; margin-left:10px"></i>
-          <i v-if="item.type==='add'" class="el-icon-user" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===1" class="el-icon-user" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===2" class="el-icon-s-comment" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===3" class="el-icon-close" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===4" class="el-icon-error" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===5" class="el-icon-user" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===6" class="el-icon-chat-line-round" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===7" class="el-icon-chat-round" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===8" class="el-icon-close" style="margin-top:40%; margin-left:10px"></i>
+          <i v-if="item.type===9" class="el-icon-error" style="margin-top:40%; margin-left:10px"></i>
       </el-col>
       <el-col style="margin-top:10px">
-          <el-row><div class="title">{{item.name}}{{item.content}}</div></el-row>
+          <el-row><div class="title">{{item.origin_user}}{{item.content}}</div></el-row>
           <el-row><div class="details">{{item.detail}}</div></el-row>
           <br>
           <el-row>
-            <el-col span="14"><time class="time">{{item.date}}</time></el-col>
-              <el-button type="text" icon="el-icon-view">查看</el-button>
+            <el-col span="14"><time class="time">{{item.time}}</time></el-col>
+              <el-button type="text" icon="el-icon-close" @click="deleteMessage(item.id)">删除</el-button>
               <el-divider direction="vertical"></el-divider>
-              <el-button type="text" icon="el-icon-check">标为已读</el-button>
+              <el-button v-if="item.status===0" type="text" icon="el-icon-check" @click="readMessage(item.id,item.status)">标为已读</el-button>
+              <el-button v-if="item.status===1" type="text" disabled="true">已读</el-button>
           </el-row>   
       </el-col>
     </el-container>
@@ -27,12 +33,37 @@
 </template>
 
 <script>
+import {readMessage, deleteMessage} from '../api/api'
 export default {
   name: 'messageCard',
   props: {
     messages: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    //单个消息已读
+    readMessage(messageId,messageStatus){
+      if(messageStatus===1) messageStatus=0
+      else if(messageStatus===0) messageStatus=1
+      readMessage(messageId,messageStatus).then(response => {
+        if(response.status===200){
+          console.log(response)
+        }else{
+          console.log(response)
+        }
+      })
+    },
+    //单个消息删除
+    deleteMessage(messageId){
+      deleteMessage(messageId).then(response => {
+        if(response.status===200){
+          console.log(response)
+        }else{
+          console.log(response)
+        }
+      })
     }
   }
 }
